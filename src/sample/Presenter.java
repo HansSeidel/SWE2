795a5bbba3565de5e52f02b8ac93bac9168d9449
Hans Seidel <42345214+HansSeidel@users.noreply.github.com>
2021-03-08 17:11:18 +0100

    Data manipulation without binding accomplished

--- src/sample/Presenter.java
+++ src/sample/Presenter.java
@@ -5,6 +5,8 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 import java.awt.*;
@@ -13,13 +15,16 @@ public class Presenter {
 
     public JFXTextField username_jfxtf;
     public JFXPasswordField password_jfxtf;
-    public JFXButton button_jfxbtn;
+    public JFXButton submit_jfxbtn;
+    public Label username_lbl;
 
     public void submit(){
         //Will be moved into ViewModel class
         //Will be checked with actual values
-        if(username_jfxtf.getText().equals("admin") && password_jfxtf.getText().equals("admin")){
-            button_jfxbtn.getStyleableNode().setDisable(true);
+        if(username_jfxtf.getText().equals("admin") && password_jfxtf.getText().equals("admin") && !submit_jfxbtn.disableProperty().getValue()){
+            submit_jfxbtn.disableProperty().setValue(true);
+            username_lbl.fontProperty().setValue(Font.getDefault());
+            username_lbl.textProperty().setValue(username_jfxtf.getText());
         }
     }
 
